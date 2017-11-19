65d786b235a0076207abfea16cf26907c3da1d17
l94ti <l94tiboe@gmail.com>
2017-11-19 14:20:19 +0200

    master - init

---
+++ src/main/java/com/app/Launcher.java
@@ -0,0 +1,23 @@
+package com.app;
+
+import com.gluonhq.particle.application.ParticleApplication;
+import javafx.scene.Scene;
+import static org.controlsfx.control.action.ActionMap.actions;
+
+public class Launcher extends ParticleApplication {
+
+    public Launcher() {
+        super("Gluon Desktop Application");
+    }
+
+    @Override
+    public void postInit(Scene scene) {
+        scene.getStylesheets().add(Launcher.class.getResource("style.css").toExternalForm());
+
+        setTitle("Gluon Desktop Application");
+
+        getParticle().buildMenu("File -> [exit]", "Help -> [about]");
+        
+        getParticle().getToolBarActions().addAll(actions("---", "about", "exit"));
+    }
+}
